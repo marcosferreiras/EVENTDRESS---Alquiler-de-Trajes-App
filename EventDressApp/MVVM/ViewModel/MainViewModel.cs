f16bbc24438d3b696dd03baef0a161512fe282df
Sherman Perez <shermanperez0@gmail.com>
2024-12-13 17:45:21 -0400

    Add project files.

---
+++ EventDressApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using EventDressApp.Core;
+
+namespace EventDressApp.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand ClientesViewCommand { get; set; }
+
+        public HomeViewModel HomeVm{ get; set; }  
+        public ClientesViewModel ClientesVm{ get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {  
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() { 
+            HomeVm = new HomeViewModel();
+            ClientesVm = new ClientesViewModel();
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+
+            ClientesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ClientesVm;
+            });
+        }
+    }
+}
